4484a5a1d1e066952fe9ecee0378f18bea9cbef1
dylanEstiam <dylan.regnault@estiam.com>
2021-05-19 11:51:09 +0200

    structure data return

--- Back/app/controllers/especeController.js
+++ Back/app/controllers/especeController.js
@@ -1,10 +1,14 @@
-const especes = require('../models/index').espece;
+const especes = require('../models/index').personnel;
 const catchError = require('../models/index').catchFunction;
 
 exports.index = async (req, res) => {
     try {
         const allEspeces = await especes.findAll();
-        res.status(200).send(allEspeces);
+        res.status(200).send({
+            success: true,
+            message: "liste des espèces",
+            data: allEspeces
+        });
     } catch(err) {
         catchError(err, res);
     }

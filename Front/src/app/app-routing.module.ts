7d17555398dc2f70a23541a68ff6d5e4818002b5
Kahbei <mengus.gary@gmail.com>
2021-05-19 18:04:08 +0200

    Add route, route link and change css

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -3,16 +3,51 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AlerteValidationCandidaturesComponent } from './components/alerte-validation-candidatures/alerte-validation-candidatures.component';
 import { AlertHomeComponent } from './components/alert-home/alert-home.component';
+import { CandidatureMotivationComponent } from './components/candidature-motivation/candidature-motivation.component';
+import { LotsComponent } from './components/lots/lots.component';
+import { LotsDetailsComponent } from './components/lots-details/lots-details.component';
+import { PersonnelsComponent } from './components/personnels/personnels.component';
+import { SitesComponent } from './components/sites/sites.component';
+import { TaxonomieComponent } from './components/taxonomie/taxonomie.component';
+import { TaxonomieDetailsComponent } from './components/taxonomie-details/taxonomie-details.component';
 
 const routes: Routes = [
-  {
-    path: 'candidatures',
-    component: AlerteValidationCandidaturesComponent,
-  },
   {
     path: '',
     pathMatch: 'full',
     component: AlertHomeComponent,
+  },
+  {
+    path: 'candidature/list',
+    component: AlerteValidationCandidaturesComponent,
+  },
+  {
+    path: 'candidature',
+    component: CandidatureMotivationComponent,
+  },
+  {
+    path: 'lots',
+    component: LotsComponent,
+  },
+  {
+    path: 'lots/details',
+    component: LotsDetailsComponent,
+  },
+  {
+    path: 'personnels',
+    component: PersonnelsComponent,
+  },
+  {
+    path: 'sites',
+    component: SitesComponent,
+  },
+  {
+    path: 'taxonomie',
+    component: TaxonomieComponent,
+  },
+  {
+    path: 'taxonomie/details',
+    component: TaxonomieDetailsComponent,
   },
 ];
 

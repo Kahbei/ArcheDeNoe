0d94ca802df223845dbf0ce3a71d697311952617
dylanEstiam <dylan.regnault@estiam.com>
2021-05-26 16:49:13 +0200

    Finish alerte controller and route

--- Back/app/controllers/alerteController.js
+++ Back/app/controllers/alerteController.js
@@ -17,21 +17,24 @@ exports.getAll = async (req, res) => {
 exports.createOne = async (req, res) => {
   try {
     const sRet = [];
-    if (!req.body.idCandidature) {
-      sRet.push("Il manque le paramètre idCandidature");
-    } else if (typeof req.body.idCandidature !== "number") {
-      sRet.push("idCandidature doit être de type number");
+    if (!req.body.idEspece) {
+      sRet.push("Il manque le paramètre idEspece");
+    } else if (typeof req.body.idEspece !== "number") {
+      sRet.push("idEspece doit être de type number");
     }
-    if (!req.body.role) {
-      sRet.push("Il manque le paramètre role");
-    } else if (typeof req.body.role !== "string") {
-      sRet.push("role doit être de type string");
+
+    if (!req.body.libelle) {
+      sRet.push("Il manque le paramètre libelle");
+    } else if (typeof req.body.libelle !== "string") {
+      sRet.push("libelle doit être de type string");
     }
-    if (!req.body.narrateur) {
-      sRet.push("Il manque le paramètre narrateur");
-    } else if (typeof req.body.narrateur !== "string") {
-      sRet.push("narrateur doit être de type string");
+
+    if (!req.body.description) {
+      sRet.push("Il manque le paramètre description");
+    } else if (typeof req.body.description !== "string") {
+      sRet.push("description doit être de type string");
     }
+
     if (sRet.length > 0) {
       throw {
         status: 400,
@@ -42,9 +45,9 @@ exports.createOne = async (req, res) => {
     }
 
     const newActeur = await alertes.create({
-      id_candidature: req.body.idCandidature,
-      role: req.body.role,
-      narrateur: req.body.narrateur,
+      id_espece: req.body.idEspece,
+      libelle: req.body.libelle,
+      description: req.body.description,
     });
 
     if (!newActeur) {
@@ -57,8 +60,160 @@ exports.createOne = async (req, res) => {
 
     res.status(200).send({
       success: true,
-      message: "Acteur créé avec succès",
+      message: "Alerte créé avec succès",
       data: newActeur,
+    });
+  } catch (error) {
+    catchError(error, res);
+  }
+};
+
+exports.readOne = async (req, res) => {
+  try {
+    sRet = [];
+    if (!req.body.idAlerte) {
+      sRet.push("Il manque le paramètre idAlerte");
+    } else if (typeof req.body.idAlerte !== "number") {
+      sRet.push("idEspece doit être de type number");
+    }
+    if (sRet.length > 0) {
+      throw {
+        status: 400,
+        success: false,
+        message: "Erreur dans le paramètre idAlerte",
+        data: sRet,
+      };
+    }
+
+    const alerte = await alertes.findOne({
+      where: {
+        id_alerte: req.body.idAlerte,
+      },
+    });
+
+    if (!alerte) {
+      throw {
+        status: 404,
+        success: false,
+        message: "Alerte non trouvée",
+      };
+    }
+
+    res.status(200).send({
+      success: true,
+      message: "Alerte trouvée avec succès",
+      data: alerte,
+    });
+  } catch (error) {
+    catchError(error, res);
+  }
+};
+
+exports.updateOne = async (req, res) => {
+  try {
+    const sRet = [];
+    if (!req.body.idAlerte) {
+      sRet.push("Il manque le paramètre idAlerte");
+    } else if (typeof req.body.idAlerte !== "number") {
+      sRet.push("idEspece doit être de type number");
+    }
+
+    if (!req.body.idEspece) {
+      sRet.push("Il manque le paramètre idEspece");
+    } else if (typeof req.body.idEspece !== "number") {
+      sRet.push("idEspece doit être de type number");
+    }
+
+    if (!req.body.libelle) {
+      sRet.push("Il manque le paramètre libelle");
+    } else if (typeof req.body.libelle !== "string") {
+      sRet.push("libelle doit être de type string");
+    }
+
+    if (!req.body.description) {
+      sRet.push("Il manque le paramètre description");
+    } else if (typeof req.body.description !== "string") {
+      sRet.push("description doit être de type string");
+    }
+
+    if (sRet.length > 0) {
+      throw {
+        status: 400,
+        success: false,
+        message: "Problème dans les paramètres",
+        data: sRet,
+      };
+    }
+
+    await alertes.update(
+      {
+        id_espece: req.body.idEspece,
+        libelle: req.body.libelle,
+        description: req.body.description,
+      },
+      {
+        where: {
+          id_alerte: req.body.idAlerte,
+        },
+      }
+    );
+    const updatedAlert = await alertes.findOne({
+      where: {
+        id_alerte: req.body.idAlerte
+      }
+    })
+    if (!updatedAlert) {
+      throw {
+        status: 500,
+        success: false,
+        message: "Erreur lors de l'update de l'alerte",
+      };
+    }
+
+    res.status(200).send({
+      success: true,
+      message: `Alerte mise à jour`,
+      data: updatedAlert
+    });
+  } catch (error) {
+    catchError(error, res);
+  }
+};
+
+exports.deleteOne = async (req, res) => {
+  try {
+    sRet = [];
+    if (!req.body.idAlerte) {
+      sRet.push("Il manque le paramètre idAlerte");
+    } else if (typeof req.body.idAlerte !== "number") {
+      sRet.push("idEspece doit être de type number");
+    }
+    if (sRet.length > 0) {
+      throw {
+        status: 400,
+        success: false,
+        message: "Erreur dans le paramètre idAlerte",
+        data: sRet,
+      };
+    }
+
+    const deletedAlerte = await alertes.destroy({
+      where: {
+        id_alerte: req.body.idAlerte,
+      },
+    });
+
+    if (!deletedAlerte) {
+      throw {
+        status: 500,
+        success: false,
+        message: "Erreur lors de la suppression",
+      };
+    }
+
+    res.status(200).send({
+      success: true,
+      message: `Alerte ${req.body.idAlerte} supprimée`,
     });
   } catch (error) {
     catchError(error, res);
--- Back/app/routes/alerteRoute.js
+++ Back/app/routes/alerteRoute.js
@@ -6,6 +6,9 @@ const controller = require("../controllers/alerteController");
 
 // récupère toutes les espèces
 router.get("/", controller.getAll);
-router.post("/createOne", controller.createOne)
+router.post("/createOne", controller.createOne);
+router.get("/getOne", controller.readOne);
+router.patch("/updateOne", controller.updateOne);
+router.delete("/deleteOne", controller.deleteOne);
 
 module.exports = router;
